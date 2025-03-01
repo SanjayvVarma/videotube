040df3d93c81a50eeae5f04186adc38a07cc5469
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-03-01 08:24:17 +0530

    fix formatting in index.js for consistency

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import dbConnection from "./database/dbConnection.js";
 
 dbConnection()
 
-app.on("error", (error)=>{
+app.on("error", (error) => {
     console.log("ERROR", error);
     throw error;
 })
 
-app.listen(process.env.PORT || 8080, ()=>{
-console.log("Server is running at port ", process.env.PORT );
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Server is running at port ", process.env.PORT);
 })
